1a6742d42796040c1e8228bb5c24f16ab2aa799d
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 00:16:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(array_of_name)
         badge_maker(name)
     end
 end
+
+def assign_rooms(speaker_list)
+    rooms = Array(7,"")
+    counter = 0
+    speaker_list.each do |person|
+        rooms[counter] = person
+        counter += 1
+    end
+end
