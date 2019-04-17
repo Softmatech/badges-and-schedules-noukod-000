617043e26026ae7bfdef0578bd715b9a45791db9
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 00:23:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,7 @@ def assign_rooms(speaker_list)
     rooms = Array(7,"")
     counter = 0
     speaker_list.each do |person|
-        rooms[counter] = "Hello, #{person}! You'll be assigned to room #{rooms[counter]}!"
+        return "Hello, #{person}! You'll be assigned to room #{rooms[counter]}!"
         counter += 1
     end
-    return rooms
 end
